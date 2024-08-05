0e944a326f89a31665a266d9fb47fa7bb9f6651b
Ekter <ektermoi1@gmail.com>
2024-08-05 17:52:47 +0200

    feat: tuple syntax for multi-call (not functional yet)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import platform
 import time
+from typing import Union, Callable
 
 from actuators import Actuator
 
@@ -22,22 +23,42 @@ class Robot:
 
 class RunnerThread:
     class CallQueue:
-        def __init__(self, th: "RunnerThread") -> None:
-            self.th = th
+        def __init__(self, _th: "RunnerThread") -> None:
+            self.th = _th
             self.calling_queue = []
 
-        def __or__(self, fn):
-            self.calling_queue.append(fn)
+        def __or__(self, fn: Union[tuple[Callable], Callable]):
+            self.calling_queue.append((fn))
             return self
 
         def __iter__(self):
-            for call in self.calling_queue:
-                yield call
+            """
+            Iteration over the calling queue to get all methods in order
+
+            Indirect iteration to allow for dynamic changes to the queue
+            during execution
+            """
+            call_n = 0
+            while call_n < len(self.calling_queue):
+                yield self.calling_queue[call_n]
+                call_n += 1
 
         def __len__(self):
             return len(self.calling_queue)
 
+        def __sub__(self, fn: Union[int, tuple[Callable], Callable]):
+            if isinstance(fn, int):
+                self.calling_queue = self.calling_queue[:-fn]
+                return self
+            self.calling_queue.remove(fn)
+            return self
+
+        def __lt__(self, *args):
+            self.th.initial_args = args
+            return self
+
     def __init__(self, period: float = -1):
+        self.initial_args = []
         self.callback = RunnerThread.CallQueue(self)
         if PLATFORM == "MicroPython":
             self.thread = threading.start_new_thread(self._run, ())
@@ -48,30 +69,58 @@ class RunnerThread:
 
     def _run(self):
         print(len(self.callback))
-        a=[]
+        a = self.initial_args
         for call in self.callback:
-            a = [call(*a)]
+            a = [(call_(*a) for call_ in call) if isinstance(call, tuple) else call(*a)]
 
 
 th = RunnerThread(-1)
 
 
 def f1():
-    print("1")
+    print(1)
     return 2
 
 
-
 def f2(u):
-    print(2*u)
-    return 2*u
+    print(2)
+    print(2 * u)
+    return 2 * u
 
 
 def f3(k):
+    print(3)
     print(k)
 
 
 time.sleep(1)
 th.callback | f1 | f2 | f2 | f3
-print("end")
+print("----------")
+th._run()
+
+
+def f4(s):
+    print("4")
+    return s
+
+
+def f5(k):
+    print(5)
+    return k * 5
+
+def f52(k):
+    print("52")
+    return k * 10
+
+
+def f6(*args):
+    print(6)
+    print(args)
+
+
+time.sleep(1)
+th.callback - 10
+th.callback < "a"
+th.callback | f4 | (f5, f52) | f6
+print("----------")
 th._run()
