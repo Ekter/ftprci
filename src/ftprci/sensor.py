7ce282bfe370afabb236532ef28e4dd4747562bd
Ekter <ektermoi1@gmail.com>
2024-08-16 23:48:09 +0200

    docs: better docstrings on sensor bump version to 0.2.0 because of breaking changes on interface

--- src/ftprci/sensor.py
+++ src/ftprci/sensor.py
@@ -9,29 +9,28 @@ class Sensor(abc.ABC):
     Abstract base class for sensors.
 
     This class defines the interface that should be implemented by all sensors.
-    The sensor generally behaves like a wrapper around Interface, waiting for commands
+    The sensor generally behaves like a wrapper around `Interface`, waiting for commands
     and returning raw data.
 
     Abstract methods:
-        * read: read data from the sensor.
+    * `read`: read data from the sensor.
 
-    Use __init__ to initialize the sensor if needed.
+    Use `__init__` to initialize the sensor if needed.
 
     Member classes:
-        * OutputTypes:
-            Class of possible types of numbers.
-            Utility to forge complex RawData structures.
-        * RawData:
-            Struct for sensors output values. Each sensor should implement its own
-            RawData class potentially using OutputTypes if needed.
+    * `OutputTypes`:
+        Class of possible types of numbers.
+        Utility to forge complex `RawData` structures.
+    * `RawData`:
+        Struct for sensors output values. Each sensor should implement its own
+        `RawData` class potentially using `OutputTypes` if needed.
     """
 
-    class OutputTypes():
+    class OutputTypes:
         """
         Possible return types of the read method.
 
         Utility to forge complex RawData structures.
-
         """
         Vector3 = collections.namedtuple('Vector3', 'x y z')
         Vector2 = collections.namedtuple('Vector2', 'x y')
@@ -61,13 +60,13 @@ class Sensor(abc.ABC):
 
 class Accelerometer(Sensor):
     class RawData:
-        def __init__(self, acc):
+        def __init__(self, acc=(0, 0, 0)):
             self.acc = Sensor.OutputTypes.Vector3(*acc)
 
 
 class Gyrometer(Sensor):
     class RawData:
-        def __init__(self, acc):
+        def __init__(self, acc=(0, 0, 0)):
             self.acc = Sensor.OutputTypes.Vector3(*acc)
 
 
@@ -78,14 +77,30 @@ class Encoder(Sensor):
 
 
 class AccGyro(Accelerometer, Gyrometer):
+    """
+    Class for one-board dual sensors containing an accelerometer and a gyroscope.
+
+    Example: LSM6
+
+    `RawData` is `(Vector3, Vector3)`
+    """
     class RawData:
-        def __init__(self, acc, gyro): # ugly, I want to find something better for this
+        """
+        Class for storing the output of combined accelerometers and gyroscopes.
+
+        Just has the two of them combined.
+
+        Members:
+            * acc:
+                `Vector3` of acceleration(a_x, a_y, a_z)
+            * gyro:
+                `Vector3` of angular speed(w_x, w_y, w_z)
+        """
+        def __init__(self, acc=(0, 0, 0), gyro=(0, 0, 0)):
+            # ugly, I want to find something better for this
             # but I see no trivial solution that could work for a double encoder for example
             self.acc = Sensor.OutputTypes.Vector3(*acc)
             self.gyro = Sensor.OutputTypes.Vector3(*gyro)
-
-    def __init__(self):
-        super().__init__()
 
 
 class LSM6(AccGyro):
@@ -94,6 +109,8 @@ class LSM6(AccGyro):
 
     The address should be 0x6A or 0x6B depending on the SDO/SA0 connection for
     the Sigi robot.
+
+    `RawData` is `(Vector3, Vector3)`
     """
 
     class Regs(enum.Enum):
