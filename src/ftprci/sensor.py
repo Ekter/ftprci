2faadea01a794babee2ba1412876ee13685afca7
Ekter <ektermoi1@gmail.com>
2025-01-09 22:23:47 +0100

    feat: change who am i print to binary

--- src/ftprci/sensor.py
+++ src/ftprci/sensor.py
@@ -521,7 +521,7 @@ class LSM9DS1(AccGyroMag):
         sensor_identity = self.accgyro.read(address=LSM9DS1.RegsAccGyro.WHO_AM_I, max_bytes=1)[0]
         status_l = self.accgyro.read(address=LSM9DS1.RegsAccGyro.STATUS_REG_L, max_bytes=1)[0]
         status_h = self.accgyro.read(address=LSM9DS1.RegsAccGyro.STATUS_REG_H, max_bytes=1)[0]
-        print(f"checked sensor {sensor_identity:02x}, got status {status_l:08b} and {status_h:08b}")
+        print(f"checked sensor {sensor_identity:08b}, got status {status_l:08b} and {status_h:08b}")
         # TODO nice print of status & check of values
 
 
